efb596b3e4236f065fc9f9043b305a66e43b94c4
comision20Formar <escobardebo@gmail.com>
2023-05-16 20:01:17 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const process = require('process');
+const calculadora = require('./calculadora');
+
+const operaciones =['sumar','restar','multiplicar','dividir'];
+const operacion = process.argv[2];
+const numeroA = +process.argv[3];
+const numeroB = +process.argv[4];
+let resultado = 0;
+
+if(operaciones.includes(operacion)){
+if(operacion === "sumar"){
+    resultado = calculadora.sumar(numeroA,numeroB)
+} else if(operacion === "restar"){
+resultado = calculadora.restar(numeroA,numeroB)
+} else if(operacion === "multiplicar"){
+    resultado.multiplicar(numeroA,numeroB)
+} else if(operacion === "dividir"){
+    resultado.dividir(numeroA,numeroB)
+} 
+console.log(resultado);
+
+} else {
+    console.log("error: La operación no es válida");
+}
